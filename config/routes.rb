adb760342561abd43c9bb948b038fd613ce244d5
Emma Rünzel <emma@ruenzel.de>
2022-11-30 10:04:07 -0500

    you can sign up and log in now

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'dashboard#admin'
   # get 'dashboard/admin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
