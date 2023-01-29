3291a66e766c403459c1ba786595b34e6f313134
Emma Rünzel <emma@ruenzel.de>
2023-01-29 22:15:54 +0100

    Navbar logic and log in fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
+# app/controllers/application_controller.rb
+
 class ApplicationController < ActionController::Base
   before_action :set_should_render_navbar
-
+  
   def set_should_render_navbar
     @should_render_navbar = false
   end
