ce43b2c191ac6a9c5547baa692001ef22d9d5de4
Emma A. A. Rünzel <emma@ruenzel.de>
2023-07-30 21:08:00 +0200

    Add Happy Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,44 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Clean Database 🧼"
 User.destroy_all
+HappyThings.destroy_all
 
-lea = User.create (
+lea = User.create(
   first_name: 'Lea',
   last_name: 'Balkenhol',
-  email: 'lea.balkenhol@outlook.de'
+  email: 'lea.balkenhol@outlook.de',
+  password: '123456'
 )
 
-lea = User.create (
+emma = User.create(
   first_name: 'Emma',
   last_name: 'Rünzel',
-  email: 'emma@test.com'
+  email: 'emma@test.com',
+  password: '123456'
 )
+
+puts "Create Users 🙌"
+
+# Seed HappyThings for Lea for the last 5 days
+5.times do |i|
+  happy_thing = HappyThing.create(
+    user: lea,
+    content: "Theater spielen #{i + 1}!",
+    date: Date.today - i,
+    time: Time.now - (i * 2).hours
+  )
+end
+
+# Seed HappyThings for Emma for the last 3 days
+3.times do |i|
+  happy_thing = HappyThing.create(
+    user: emma,
+    content: "Mit meinen Geschwistern sein #{i + 1}!",
+    date: Date.today - i,
+    time: Time.now - (i * 3).hours
+  )
+end
+
+puts "Create HappyThings 🙌"
