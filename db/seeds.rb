ba749d223a6142e5e50321c1c7ffd1773c9ee686
Emma A. A. Rünzel <emma@ruenzel.de>
2023-07-30 21:52:53 +0200

    Add more Happy Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Seed data for Five Things App
 
+# Gems
+require "faker"
+
+# Clear existing data
 User.destroy_all
 Thing.destroy_all
 
-puts "Creating Users ..."
 
-lea = User.create(
+# Create Users
+users = []
+users << User.create(
   first_name: 'Lea',
   last_name: 'Balkenhol',
-  email: 'lea.balkenhol@outlook.de'
+  email: 'lea@test.com',
+  password: 'password'
 )
-
-emma = User.create(
+users << User.create(
   first_name: 'Emma',
   last_name: 'Rünzel',
-  email: 'emma@test.com'
+  email: 'emma@test.com',
+  password: '123456'
 )
+puts "Create Users 🙌"
 
-puts "Creating Things ..."
 
-thing_one = Thing.create(
-  date: DateTime.new(2023,9,1,17),
-  first: '1. Coffee in the morning',
-  second: '2. Three cats looking at me with big eyes',
-  third: '3. Message from Lea',
-  forth: '4. Tea with lemon',
-  fifth: '5. Hugging someone after they lost somebody',
-  user_id: emma.id
-)
-
-thing_two = Thing.create(
-  date: DateTime.new(2023,9,1,17),
-  first: '1. Run in the morning',
-  second: '2. One dog looking at me with big eyes',
-  third: '3. Message from Emma',
-  forth: '4. Black Tea',
-  fifth: '5. Hugging someone after a long day',
-  user_id: lea.id
-)
+# Create HappyThings
+happy_things = []
+users.each do |user|
+  5.times do |i|
+    happy_things << HappyThing.create(
+      user: user,
+      content: "Placeholder Happy Thing #{i + 1} for #{user.full_name} on Day #{i + 1}",
+      date: Date.today - i,
+      time: Time.now - (i * 2).hours
+    )
+  end
+end
+puts "Create HappyThings 🙌"
 
 puts "Done ✅"
