c6224f66d7909990b7a3ef58bdddfff558dd910b
Emma A. A. Rünzel <emma@ruenzel.de>
2023-08-11 10:12:21 +0200

    Update Tests to match one page edit & update

--- test/system/happy_things_test.rb
+++ test/system/happy_things_test.rb
@@ -10,28 +10,6 @@ class HappyThingsTest < ApplicationSystemTestCase
     login_as @user
   end
 
-  test "Creating a new happy thing" do
-    # When we visit the HappyThings#index page
-    # we expect to see a title with the text "HappyThings"
-    visit happy_things_path
-    assert_selector "h1", text: "Happy Things"
-
-    # When we click on the link with the text "New happy thing"
-    # we expect to land on a page with the title "New happy thing"
-    click_on "New happy thing"
-    assert_selector "h1", text: "New happy thing"
-
-    # When we fill in the name input with "Capybara happy thing"
-    # and we click on "Create HappyThing"
-    fill_in "Name", with: "Create happy thing"
-    click_on "Create happy thing"
-
-    # We expect to be back on the page with the title "Happy Things"
-    # and to see our "Capybara happy thing" added to the list
-    assert_selector "h1", text: "Happy Things"
-    assert_text "Happy Thing was successfully created."
-  end
-
   test "Showing a happy thing" do
     visit happy_things_path
     click_link @happy_thing.title
@@ -39,16 +17,52 @@ class HappyThingsTest < ApplicationSystemTestCase
     assert_selector "h1", text: @happy_thing.title
   end
 
-  test "Updating a happy thing" do
+  test "Creating a new happy thing" do
+    # visit happy_things_path
+    # assert_selector "h1", text: "Happy Things"
+
+    # click_on "New happy thing"
+    # assert_selector "h1", text: "New happy thing"
+
+    # fill_in "Title", with: "Create happy thing"
+    # click_on "Create happy thing"
+
+    # assert_selector "h1", text: "Happy Things"
+    # assert_text "Happy Thing was successfully created."
+
     visit happy_things_path
     assert_selector "h1", text: "Happy Things"
 
-    # click_on "Edit", match: :one
-    # assert_selector "h1", text: "Edit happy thing"
-    find(".edit-link", match: :first).click
-    assert_selector "h1", text: "Edit happy thing"
+    click_on "New quote"
+    fill_in "Title", with: "New happy thing"
 
-    fill_in "happy_thing[title]", with: "Updated happy thing"
+    assert_selector "h1", text: "Happy Things"
+    click_on "Create happy thing"
+
+    assert_selector "h1", text: "Happy Things"
+    assert_text "Happy Thing was successfully created."
+  end
+
+  test "Updating a happy thing" do
+    # visit happy_things_path
+    # assert_selector "h1", text: "Happy Things"
+
+    # find(".edit-link", match: :first).click
+    # assert_selector "h1", text: "Edit happy thing"
+
+    # fill_in "happy_thing[title]", with: "Updated happy thing"
+    # click_on "Update happy thing"
+
+    # assert_selector "h1", text: "Happy Things"
+    # assert_text "Updated happy thing"
+
+    visit happy_things_path
+    assert_selector "h1", text: "Happy Things"
+
+    click_on "Edit", match: :first
+    fill_in "Title", with: "Updated happy thing"
+
+    assert_selector "h1", text: "Happy Things"
     click_on "Update happy thing"
 
     assert_selector "h1", text: "Happy Things"
