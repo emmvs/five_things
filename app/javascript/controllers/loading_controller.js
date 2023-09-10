c9cf81a109803d092518ca223f1e5e09d923b56f
Emma A. A. Rünzel <emma@ruenzel.de>
2023-09-10 09:36:32 +0200

    Update photo path on Dashboards#Index

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -3,5 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="loading"
 export default class extends Controller {
   connect() {
+    console.log("🪩")
+    // this.element.addEventListener("animationend", this.removeElement);
   }
+
+  // disconnect() {
+  //   this.element.removeEventListener("animationend", this.removeElement);
+  // }
+
+  // removeElement = () => {
+  //   this.element.remove();
+  // };
 }
