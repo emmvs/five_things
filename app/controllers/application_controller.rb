c1a2c2291fab482e418c48943426e9b7d0d1f6b2
Emma A. A. Rünzel <emma@ruenzel.de>
2023-09-20 09:50:45 -0400

    Fix Devise Parameters To Be Permitted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@
 
 class ApplicationController < ActionController::Base
   before_action :set_should_render_navbar
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Add Sleep to see changes
   # before_action -> { sleep 3 }
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 
   def set_should_render_navbar
     @should_render_navbar = false
