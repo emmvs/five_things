aba295d64256372a212dcad274233ff4affae326
Emma A. A. Rünzel <emma@ruenzel.de>
2023-10-04 15:13:54 -0400

    Add Section to Display Happy Things From One Year Ago in Dashboard View

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -9,6 +9,9 @@ class DashboardsController < ApplicationController
       start_time: Time.zone.now.beginning_of_month.beginning_of_week..Time.zone.now.end_of_month.end_of_week
     )
     @random_poem = PoetryService.get_random_poem_by_author("Emily Dickinson")
+
+    # Retrieve Happy Things from one year ago
+    @happy_things_one_year_ago = @happy_things.where("DATE(start_time) = ?", 1.year.ago.to_date).order(created_at: :desc)
   end
 
   # def retrieve_poem
