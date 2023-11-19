5824a931004cc1e28359ae7a09273470e08f2075
Emma A. A. Rünzel <emma@ruenzel.de>
2023-11-19 22:12:52 +0100

    Refactor HappyThing Model & Integrate AI Titles in Views

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -9,16 +9,6 @@ class DashboardsController < ApplicationController
     @happy_things_one_year_ago = HappyThing.where("DATE(start_time) = ?", 1.year.ago.to_date).order(created_at: :desc)
 
     @random_poem = fetch_daily_poem
-
-    client = OpenAI::Client.new
-      chaptgpt_response = client.chat(
-        parameters: {
-          model: "gpt-3.5-turbo",
-          messages: [{
-            role: "user",
-            content: "Give me a simple thing to do that would make someone happy who is sufferring with depression. Give me only the title of the happy thing, without any of your own answer like 'Here is a simple happy thing'. Please make sure that the things are special and good for intelligent people who love, literature, cute animals, being outside and being surround by loved ones. It should fit into a text like so: 'Something to make you happy is\n todo.'"}]
-        })
-      @ai_happy_thing = chaptgpt_response["choices"][0]["message"]["content"]
   end
 
   private
--- app/models/happy_thing.rb
+++ app/models/happy_thing.rb
@@ -11,6 +11,20 @@ class HappyThing < ApplicationRecord
     self.start_time = DateTime.now
   end
 
+  def ai_title
+    Rails.cache.fetch("#{cache_key_with_version}/content") do
+      client = OpenAI::Client.new
+      chaptgpt_response = client.chat(
+        parameters: {
+          model: "gpt-4",
+          messages: [{
+            role: "user",
+            content: "Give me a simple thing to do that would make someone happy who is suffering from depression. Give me only the title, without any of your own weird metaphors or answers like 'Here is a simple happy thing.' Please ensure that the things are unique and suitable for intelligent people who need their brains stimulated by good things. It should fit into a text like so: 'Something to make you happy is\n todo.'"}]
+        })
+      @ai_happy_thing = chaptgpt_response["choices"][0]["message"]["content"]
+    end
+  end
+
   private
 
   def start_time_present?
