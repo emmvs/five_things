9be5f97794951a293cd8c30a9f375654f58b723b
Emma A. A. Rünzel <emma@ruenzel.de>
2023-12-25 18:21:37 +0100

    Add fetch_daily_quote Method to DashboardsController

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -10,6 +10,7 @@ class DashboardsController < ApplicationController
     @happy_things_one_year_ago = HappyThing.where("DATE(start_time) = ?", 1.year.ago.to_date).order(created_at: :desc)
 
     @random_poem = fetch_daily_poem
+    @random_quote = fetch_daily_quote
   end
 
   private
@@ -18,5 +19,11 @@ class DashboardsController < ApplicationController
     Rails.cache.fetch("random_poem", expires_in: 24.hours) do
       PoetryService.get_random_poem
     end
+  end
+
+  def fetch_daily_quote
+    quote_service = QuoteService.new('happiness')
+    quote = quote_service.fetch_random_quote
+    puts quote unless quote.nil?
   end
 end
