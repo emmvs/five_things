b5538d4317ed5fb59a4f05d836b36e78b0e5e607
Emma A. A. Rünzel <emma@ruenzel.de>
2023-12-28 14:02:59 +0100

    Start Fixing dashboards_controller.rb w/ Quote Generator

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -10,20 +10,20 @@ class DashboardsController < ApplicationController
     @happy_things_one_year_ago = HappyThing.where("DATE(start_time) = ?", 1.year.ago.to_date).order(created_at: :desc)
 
     @random_poem = fetch_daily_poem
-    @random_quote = fetch_daily_quote
+    # @random_quote = fetch_daily_quote
   end
 
   private
 
   def fetch_daily_poem
-    Rails.cache.fetch("random_poem", expires_in: 24.hours) do
+    # Rails.cache.fetch("random_poem", expires_in: 24.hours) do
       PoetryService.get_random_poem
-    end
+    # end
   end
 
-  def fetch_daily_quote
-    quote_service = QuoteService.new('happiness')
-    quote = quote_service.fetch_random_quote
-    puts quote unless quote.nil?
-  end
+  # def fetch_daily_quote
+  #   quote_service = QuoteService.new('happiness')
+  #   quote = quote_service.fetch_random_quote
+  #   puts quote unless quote.nil?
+  # end
 end
