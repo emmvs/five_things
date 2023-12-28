9d930a40acc7408ae908941920451220e7cd05b3
Emma A. A. Rünzel <emma@ruenzel.de>
2023-12-28 14:30:10 +0100

    Implement QuoteService Producing Quotes in Rails Console

--- app/services/quote_service.rb
+++ app/services/quote_service.rb
@@ -3,21 +3,55 @@ class QuoteService < ApplicationService
 
   def initialize(category = 'happiness')
     @category = category
-    @api_key = ENV['API_NINJAS_KEY']
+    @api_key = ENV.fetch('API_NINJAS_KEY')
   end
 
-  def fetch_random_quote
-    response = make_http_request(API_URL, headers: { 'X-Api-Key' => @api_key }, query: { category: @category })
-    process_response(response)
-  # rescue StandardError => e
-  #   handle_error(e)
+  def call
+    response = HTTParty.get(API_URL, headers: { 'X-Api-Key' => @api_key }, query: { category: @category })
+    handle_response(response)
   end
 
-  def self.make_http_request(endpoint)
-    response = HTTParty.get("#{BASE_URL}#{endpoint}")
-    return nil unless response.code == 200
+  private
 
-    JSON.parse(response.body)
-    rescue JSON::ParserError, HTTParty::Error
+  def handle_response(response)
+    return JSON.parse(response.body) if response.success?
+    raise "Error fetching quote: #{response.code}"
   end
 end
+
+
+# class QuoteService < ApplicationService
+#   API_URL = 'https://api.api-ninjas.com/v1/quotes'
+
+#   def initialize(category = 'happiness')
+#     @category = category
+#     @api_key = ENV['API_NINJAS_KEY']
+#   end
+
+#   def call
+#     response = HTTParty.get(API_URL, headers: { 'X-Api-Key' => @api_key }, query: { category: @category })
+#     handle_response(response)
+#   end
+
+#   # def fetch_random_quote
+#   #   response = make_http_request(API_URL, headers: { 'X-Api-Key': @api_key }, query: { category: @category })
+#   #   process_response(response)
+#   # # rescue StandardError => e
+#   # #   handle_error(e)
+#   # end
+
+#   private
+
+#   def handle_response(response)
+#     return JSON.parse(response.body) if response.success?
+#     raise "Error fetching quote: #{response.code}"
+#   end
+
+#   def self.make_http_request(endpoint)
+#     response = HTTParty.get("#{BASE_URL}#{endpoint}")
+#     return nil unless response.code == 200
+
+#     JSON.parse(response.body)
+#     rescue JSON::ParserError, HTTParty::Error
+#   end
+# end
