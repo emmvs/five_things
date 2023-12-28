369ecd6659bf0363f1e8728937399c5e8eb5d785
Emma A. A. Rünzel <emma@ruenzel.de>
2023-12-28 15:34:01 +0100

    Clean & Refactor dashboards_controller.rb

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -29,30 +29,4 @@ class DashboardsController < ApplicationController
   def fetch_daily_poem
     PoetryService.call
   end
-
-  # def index
-  #   @should_render_navbar = true
-  #   @happy_thing = HappyThing.new
-  #   @happy_things = HappyThing.all
-  #   @happy_things_today = HappyThing.where(start_time: Date.today..Date.tomorrow).reverse.group_by(&:user)
-  #   @happy_things_of_the_last_two_days = HappyThing.where(start_time: (Date.today - 2.days)..Date.today.end_of_day).reverse.group_by(&:user)
-  #   @happy_things_one_year_ago = HappyThing.where("DATE(start_time) = ?", 1.year.ago.to_date).order(created_at: :desc)
-
-  #   @random_poem = fetch_daily_poem
-  #   @random_quote = fetch_daily_quote
-  # end
-
-  # private
-
-  # def fetch_daily_poem
-  #   PoetryService.call
-  # end
-
-  # def fetch_daily_quote
-  #   quote_infos = QuoteService.call('happiness')
-  #   quote_item = quote_infos[0]
-  #   quote_text = quote_item["quote"]
-  #   author = quote_item["author"]
-  #   "'#{quote_text}' - #{author}"
-  # end
 end
