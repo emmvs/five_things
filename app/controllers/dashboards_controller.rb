872e4b2cdb060dc22206f9d7de5a735e4fb593a2
Emma A. A. Rünzel <emma@ruenzel.de>
2023-12-28 15:33:34 +0100

    Implement Working QuoteService & PoetryService in dashboards_controller.rb

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,27 +3,56 @@ class DashboardsController < ApplicationController
 
   def index
     @should_render_navbar = true
-    @happy_thing = HappyThing.new
-    @happy_things = HappyThing.all
-    @happy_things_today = HappyThing.where(start_time: Date.today..Date.tomorrow).reverse.group_by(&:user)
-    @happy_things_of_the_last_two_days = HappyThing.where(start_time: (Date.today - 2.days)..Date.today.end_of_day).reverse.group_by(&:user)
-    @happy_things_one_year_ago = HappyThing.where("DATE(start_time) = ?", 1.year.ago.to_date).order(created_at: :desc)
-
+    set_happy_things
     @random_poem = fetch_daily_poem
-    # @random_quote = fetch_daily_quote
+    @random_quote = fetch_daily_quote
   end
 
   private
 
-  def fetch_daily_poem
-    # Rails.cache.fetch("random_poem", expires_in: 24.hours) do
-      PoetryService.get_random_poem
-    # end
+  def set_happy_things
+    @happy_things = HappyThing.all
+    @happy_things_today = happy_things_by_period(Date.today..Date.tomorrow)
+    @happy_things_of_the_last_two_days = happy_things_by_period((Date.today - 2.days)..Date.today.end_of_day)
+    @happy_things_one_year_ago = HappyThing.where("DATE(start_time) = ?", 1.year.ago.to_date).order(created_at: :desc)
   end
 
+  def happy_things_by_period(period)
+    HappyThing.where(start_time: period).reverse.group_by(&:user)
+  end
+
+  def fetch_daily_quote
+    quote_service = QuoteService.new('happiness')
+    quote_info = quote_service.call
+  end
+
+  def fetch_daily_poem
+    PoetryService.call
+  end
+
+  # def index
+  #   @should_render_navbar = true
+  #   @happy_thing = HappyThing.new
+  #   @happy_things = HappyThing.all
+  #   @happy_things_today = HappyThing.where(start_time: Date.today..Date.tomorrow).reverse.group_by(&:user)
+  #   @happy_things_of_the_last_two_days = HappyThing.where(start_time: (Date.today - 2.days)..Date.today.end_of_day).reverse.group_by(&:user)
+  #   @happy_things_one_year_ago = HappyThing.where("DATE(start_time) = ?", 1.year.ago.to_date).order(created_at: :desc)
+
+  #   @random_poem = fetch_daily_poem
+  #   @random_quote = fetch_daily_quote
+  # end
+
+  # private
+
+  # def fetch_daily_poem
+  #   PoetryService.call
+  # end
+
   # def fetch_daily_quote
-  #   quote_service = QuoteService.new('happiness')
-  #   quote = quote_service.fetch_random_quote
-  #   puts quote unless quote.nil?
+  #   quote_infos = QuoteService.call('happiness')
+  #   quote_item = quote_infos[0]
+  #   quote_text = quote_item["quote"]
+  #   author = quote_item["author"]
+  #   "'#{quote_text}' - #{author}"
   # end
 end
