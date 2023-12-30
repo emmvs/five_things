745d110b0127475d8c8a02382b5c67614e179841
Emma A. A. Rünzel <emma@ruenzel.de>
2023-12-30 23:10:44 +0100

    Add Friendship Tests to Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,39 +4,44 @@
 # require "faker"
 
 # Clear existing data
-HappyThing.destroy_all
-User.destroy_all
+# HappyThing.destroy_all
+# User.destroy_all
 
 # Create Users
-users = []
-users << User.create(
-  first_name: 'Lea',
-  last_name: 'Balkenhol',
-  email: 'lea.balkenhol@outlook.de',
-  password: 'beehappy'
-)
-users << User.create(
-  first_name: 'Emma',
-  last_name: 'Rünzel',
-  email: 'emma@test.com',
-  password: '123456'
-)
-puts "Create #{users.first.first_name} 💁🏻‍♀️ & #{users.last.first_name} 🤷🏼‍♀️"
+# users = []
+# users << User.create(
+#   first_name: 'Lea',
+#   last_name: 'Balkenhol',
+#   email: 'lea.balkenhol@outlook.de',
+#   password: 'beehappy'
+# )
+# users << User.create(
+#   first_name: 'Emma',
+#   last_name: 'Rünzel',
+#   email: 'emma@test.com',
+#   password: '123456'
+# )
+# puts "Create #{users.first.first_name} 💁🏻‍♀️ & #{users.last.first_name} 🤷🏼‍♀️"
 
 
 # Create HappyThings
-happy_things = []
-users.each do |user|
-  5.times do |i|
-    happy_things << HappyThing.create(
-      user: user,
-      body: "Happy Thing #{i + 1} for #{user.first_name} on Day #{i + 1}"
-    )
-  end
-end
-puts "Create HappyThings 🙌 🙌 🙌 🙌 🙌"
+# happy_things = []
+# users.each do |user|
+#   5.times do |i|
+#     happy_things << HappyThing.create(
+#       user: user,
+#       body: "Happy Thing #{i + 1} for #{user.first_name} on Day #{i + 1}"
+#     )
+#   end
+# end
+# puts "Create HappyThings 🙌 🙌 🙌 🙌 🙌"
 
-puts "\n== Seeding the database with fixtures, too =="
-system("bin/rails db:fixtures:load")
+# puts "\n== Seeding the database with fixtures, too =="
+# system("bin/rails db:fixtures:load")
+
+# Create Friendships
+friendships = []
+friendships << Friendship.create(wanna_be_friend: User.where(first_name: "Emma"), friend: User.where(first_name: "Name"),)
+friendships << Friendship.create(wanna_be_friend: User.last, friend: User.first)
 
 puts "Done ✅"
