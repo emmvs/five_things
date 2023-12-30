296190164296cf571b8a47c8d05482c66e1eafde
Emma A. A. Rünzel <emma@ruenzel.de>
2023-12-30 16:45:23 +0100

    Implement Word Cloud Data Aggregation in HappyThingsController

--- app/controllers/happy_things_controller.rb
+++ app/controllers/happy_things_controller.rb
@@ -43,6 +43,7 @@ class HappyThingsController < ApplicationController
 
   def analytics
     @happy_count = HappyThing.where(user: current_user).size
+    @words_for_wordcloud = get_aggregated_words(current_user, 50)
   end
 
   def show_by_date
@@ -77,5 +78,27 @@ class HappyThingsController < ApplicationController
       :status,
       :start_time
     )
+  end
+
+  def get_aggregated_words(user, word_limit)
+    one_year_ago = Date.today - 1.year
+    happy_things = happy_things_by_period(user, one_year_ago..Date.today)
+
+    all_titles = happy_things.map(&:title).join(' ')
+    words = clean_and_extract_words(all_titles)
+
+    word_count = words.each_with_object(Hash.new(0)) { |word, counts| counts[word] += 1 }
+    sorted_words = word_count.sort_by { |word, count| -count }.map(&:first)
+    sorted_words.first(word_limit)
+  end
+
+  def happy_things_by_period(user, period)
+    user.happy_things.where(start_time: period)
+  end
+
+  def clean_and_extract_words(text)
+    words = text.downcase.gsub(/[^a-z0-2\s]/i, '').split
+    stop_words = %w[und weil mit ohne the and but if or on as etc of else w to sth i]
+    words.reject { |word| stop_words.include?(word) }
   end
 end
