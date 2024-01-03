d64ed3389d195dd84f53346f948a055e09e8fd0f
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-03 11:25:12 +0100

    Update Friendship Model Relationships & Remove Status Enum

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,4 @@
 class Friendship < ApplicationRecord
-  belongs_to :wanna_be_friend, class_name: 'User'
+  belongs_to :user, class_name: 'User'
   belongs_to :friend, class_name: 'User'
-  enum status: { open: 0, pending: 1, accepted: 2 }
 end
