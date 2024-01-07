e985936c9469131a7341a239f5c8f1a65d9c21e1
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-07 15:18:04 +0100

    Add can_add_as_friend? Method to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,13 @@ class User < ApplicationRecord
   def pending_friends
     friendships.where(status: :pending)
   end
+
+  def can_add_as_friend?(current_user, potential_friend)
+    return false if current_user.friends.include?(potential_friend)
+
+    direct_friendship = current_user.friendships.find_by(friend_id: potential_friend.id)
+    inverse_friendship = potential_friend.friendships.find_by(user_id: current_user.id)
+
+    !(direct_friendship || inverse_friendship)
+  end
 end
