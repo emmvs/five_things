9b7570a5b74bca59d6b065f1f2d731e0be27590a
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-13 14:10:15 +0100

    Update Request Specs for Friendships & Users

--- spec/requests/friendships_spec.rb
+++ spec/requests/friendships_spec.rb
@@ -1,53 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe "Friendships", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/friendships/show"
-      expect(response).to have_http_status(:success)
+  let(:user) { create(:user) }
+  let(:friend) { create(:user) }
+
+  before do
+    sign_in user
+  end
+
+  describe "POST /create" do
+    it "creates a new friendship" do
+      expect {
+        post friendships_path, params: { friendship: { friend_id: friend.id } }
+      }.to change(Friendship, :count).by(1)
+      expect(response).to have_http_status(:redirect) # or :success
     end
   end
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/friendships/index"
-      expect(response).to have_http_status(:success)
+  describe "PUT /update" do
+    let(:friendship) { create(:friendship, user: user, friend: friend) }
+
+    it "updates the friendship" do
+      put friendship_path(friendship), params: { friendship: { accepted: true } }
+      expect(response).to have_http_status(:redirect) # or :success, depending on your implementation
+      expect(friendship.reload.accepted).to eq(true)
     end
   end
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/friendships/new"
-      expect(response).to have_http_status(:success)
+  describe "DELETE /destroy" do
+    let!(:friendship) { create(:friendship, user: user, friend: friend) }
+
+    it "deletes the friendship" do
+      expect {
+        delete friendship_path(friendship)
+      }.to change(Friendship, :count).by(-1)
+      expect(response).to have_http_status(:redirect) # or :success, depending on your implementation
     end
   end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/friendships/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/friendships/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/friendships/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/friendships/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  before do
+    @user = User.create!(first_name: 'Emma', email: 'emma@test.com', password: '123456')
+    sign_in @user
+  end
+
   describe "GET /index" do
     it "returns http success" do
-      get "/users/index"
+      get "/users"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/users/show"
+      user = User.create!(first_name: "Lea", email: "lea@test.com", password: "123456")
+      get "/users/#{user.id}"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
