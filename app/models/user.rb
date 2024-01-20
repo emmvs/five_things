b65c6290a5fb145daf76153bda119f5784d4f8dd
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-20 18:28:15 +0100

    Refactor HappyStreak Method f/ Efficiency & Readability

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,32 +23,31 @@ class User < ApplicationRecord
     friendships.where(status: :pending)
   end
 
+  # Calculates streak of consecutive days with happy things
+  # Each 'happy thing' is an event marked with a start_time
   def happy_streak
-    streak = 0
-    date_of_most_recent_happy_thing = nil
+    sorted_happy_things = happy_things.order(start_time: :desc)
 
-    happy_things.order(start_time: :desc).each do |happy_thing|
+    return 0 if sorted_happy_things.empty?
 
-      next if happy_thing.start_time.nil?
+    # Initialize the streak count and set the most recent date to the first item's date
+    previous_date = sorted_happy_things.first.start_time.to_date
+    streak = 1
 
+    # Iterate over the sorted happy things, starting from the second item
+    # Break the loop if the current date is not consecutive to the previous one
+    # Increment the streak and update the previous_date for the next iteration
+    sorted_happy_things.drop(1).each do |happy_thing|
       current_date = happy_thing.start_time.to_date
-
-      if date_of_most_recent_happy_thing.nil?
-        streak = 1
+      if previous_date - current_date == 1
+        streak += 1
+        previous_date = current_date
       else
-        # Check if the current HappyThing is on the consecutive day
-        # The difference between date_of_most_recent_happy_thing (January 20)
-        # and current_date (January 19) is 1 day. streak is incremented to 2
-        if (date_of_most_recent_happy_thing - current_date).to_i == 1
-          streak += 1
-        else
-          return streak
-        end
+        break
       end
-
-      date_of_most_recent_happy_thing = current_date
     end
 
     streak
   end
+
 end
