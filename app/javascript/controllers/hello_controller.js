cb51fef1e6b5c45843760d491ad66192f5f3addb
Emma A. A. Rünzel <emma@ruenzel.de>
2024-05-04 17:57:08 +0200

    Remove console.logs

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -4,7 +4,7 @@ export default class extends Controller {
   static targets = ["hello"];
   connect() {
     // this.element.textContent = "Hello World!";
-    console.log("hello from stimulus");
-    console.log(this.helloTarget);
+    // console.log("hello from stimulus");
+    // console.log(this.helloTarget);
   }
 }
