2c70323ff8c2ccbf26de268dac45bb52e681e685
Emma A. A. Rünzel <emma@ruenzel.de>
2024-05-05 18:00:16 +0200

    Create New Helpers f/ Time Based Greeting & Friendly Date of Happy Thing Partial

--- app/helpers/happy_things_helper.rb
+++ app/helpers/happy_things_helper.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
+# Helper methods for HappyThing views
 module HappyThingsHelper
+  # Formats dates as 'Today', 'Yesterday', or 'Month Day' with an ordinal suffix
+  def friendly_date(date)
+    case (Date.current - date.to_date).to_i
+    when 0
+      'Today'
+    when 1
+      'Yesterday'
+    else
+      "#{date.strftime('%B %-d')}#{date.strftime('%d').to_i.ordinal}"
+    end
+  end
 end
