a49a78e70836b72d1fe501e33732ae944c847327
Emma A. A. Rünzel <emma@ruenzel.de>
2024-05-11 17:39:16 +0200

    Fix WordCloud w/ Correct Size of Words on Analytics Page

--- app/modules/word_aggregator.rb
+++ app/modules/word_aggregator.rb
@@ -1,39 +1,36 @@
+# frozen_string_literal: true
+
+# The WordAggregator collects the most used words by the user within the last year
 module WordAggregator
   extend self
 
-  def get_aggregated_words(user, word_limit)
-    one_year_ago = Date.today - 1.year
-    happy_things_of_last_year = happy_things_by_period(user, one_year_ago..Date.today)
-
-    all_titles = happy_things_of_last_year.map(&:title).join(' ')
-    words = clean_and_extract_words(all_titles)
-
-    word_count = words.each_with_object(Hash.new(0)) { |word, counts| counts[word] += 1 }
-    sorted_word_counts = word_count.sort_by { |_word, count| -count }
-    sorted_word_counts.first(word_limit).to_h
-  end
-
-  def iterate_over_text_files(directory_path)
-    Dir.glob("#{directory_path}/*.txt").map do |file_path|
-      File.read(file_path)
-    end
+  def aggregated_words(user, word_limit)
+    texts = user_happy_things_texts(user)
+    words = extract_significant_words(texts)
+    sorted_word_counts(words, word_limit)
   end
 
   private
 
-  def happy_things_by_period(user, period)
-    user.happy_things.where(start_time: period)
+  def user_happy_things_texts(user)
+    one_year_ago = Date.today - 1.year
+    user.happy_things.where(start_time: one_year_ago..Date.today).pluck(:title).join(' ')
   end
 
-  def clean_and_extract_words(text)
-    words = text.downcase.gsub(/[^a-zA-Zäöüß0-9\s]/i, '').split
-    german_stop_words = load_stop_words(Rails.root.join('config', 'data', 'german_stopwords.txt'))
-    english_stop_words = load_stop_words(Rails.root.join('config', 'data', 'english_stopwords.txt'))
-    all_stop_words = german_stop_words + english_stop_words
-    words.reject { |word| all_stop_words.include?(word) }
+  def extract_significant_words(texts)
+    clean_words = texts.downcase.gsub(/[^a-zäöüß0-9\s]/i, '').split
+    stop_words = load_stop_words
+    clean_words.reject { |word| stop_words.include?(word) }
   end
 
-  def load_stop_words(file_path)
-    File.readlines(file_path).map(&:strip)
+  def sorted_word_counts(words, limit)
+    words.each_with_object(Hash.new(0)) { |word, counts| counts[word] += 1 }
+         .sort_by { |_word, count| -count }.first(limit).to_h
+  end
+
+  def load_stop_words
+    german = File.readlines(Rails.root.join('config', 'data', 'german_stopwords.txt')).map(&:strip)
+    english = File.readlines(Rails.root.join('config', 'data', 'english_stopwords.txt')).map(&:strip)
+    german + english
   end
 end
