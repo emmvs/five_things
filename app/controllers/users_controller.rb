21187faa7977164d16c1b6fa2753cdef1c371e96
Emma A. A. Rünzel <emma@ruenzel.de>
2024-06-02 14:10:35 +0200

    Refactor Happy Count, Happy Places & Word Cloud Data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,43 @@ class UsersController < ApplicationController
   helper FriendshipsHelper
 
   def index
-    if params[:query].present?
-      @users = User.search(params[:query]).all_except(current_user)
-    else
-      @users = User.all_except(current_user)
-    end
+    @users = fetch_users
   end
 
   def show
     @user = User.find(params[:id])
-    @happy_count = @user.happy_things.count
-    @words_for_wordcloud = WordAggregator.aggregated_words(@user, 40)
-    @visited_places_count = HappyThing.where(user_id: @user.id).distinct.count(:place)
-    @markers = @user.happy_things.geocoded.map { |ht| { lat: ht.latitude, lng: ht.longitude } }
+    @happy_count = happy_count(@user)
+    @words_for_wordcloud = words_for_wordcloud(@user)
+    @visited_places_count = visited_places_count(@user)
+    @markers = markers(@user)
+  end
+
+  private
+
+  def fetch_users
+    if params[:query].present?
+      User.search(params[:query]).all_except(current_user)
+    else
+      User.all_except(current_user)
+    end
+  end
+
+  def happy_count(user)
+    user.happy_things.count
+  end
+
+  def words_for_wordcloud(user)
+    WordAggregator.aggregated_words(user, 40)
+  end
+
+  def visited_places_count(user)
+    HappyThing.where(user_id: user.id).distinct.count(:place)
+  end
+
+  def markers(user)
+    user.happy_things.geocoded.map do |ht|
+      { lat: ht.latitude, lng: ht.longitude }
+    end
   end
 end
+ 
