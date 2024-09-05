1104aa635844ea51c3e6619e744b9fe6997118e4
Emma A. A. Rünzel <emma@ruenzel.de>
2024-09-05 13:11:26 +0200

    feat: add comments controller w/ create action & validation

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_happy_thing
+
+  def create
+    @comment = @happy_thing.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to happy_things_by_date_path(@happy_thing.start_time.strftime('%Y-%m-%d')), notice: 'Comment added successfully!'
+    else
+      redirect_to happy_things_by_date_path(@happy_thing.start_time.strftime('%Y-%m-%d')), alert: 'Failed to add comment.'
+    end
+  end
+
+  private
+
+  def set_happy_thing
+    @happy_thing = HappyThing.find(params[:happy_thing_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :happy_thing
+
+  validates :content, presence: true
 end
