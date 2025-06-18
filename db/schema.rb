fdfc15b8e3fce4dc4e6bc109d6a165ba4cb7cd36
bruno thormählen <bruno@brunos-MacBook-Air.local>
2025-06-18 23:10:27 +0200

    Add provider and uid columns to users f/ OAuth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_04_20_155126) do
+ActiveRecord::Schema[7.0].define(version: 2025_06_18_210827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -140,6 +140,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_20_155126) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "provider"
+    t.string "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
