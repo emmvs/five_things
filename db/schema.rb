45116032d2ba7590b51b50d66d0fe91c0ede2d07
brunotho <brunothormaehlen@yahoo.com>
2025-08-18 11:24:13 +0200

    Change name of daily_happy_email_sent to daily_happy_email_sent_at

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_08_18_082715) do
+ActiveRecord::Schema[7.0].define(version: 2025_08_18_091829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -145,7 +145,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_08_18_082715) do
     t.string "unconfirmed_email"
     t.string "provider"
     t.string "uid"
-    t.datetime "daily_happy_email_sent"
+    t.datetime "last_daily_happy_email_sent_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
