54c58553d8340071b56ce1f0e6e718a77bf2f2c7
brunotho <brunothormaehlen@yahoo.com>
2025-09-01 17:33:23 +0200

    Add timezone column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_06_18_210827) do
+ActiveRecord::Schema[7.0].define(version: 2025_09_01_152908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,16 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_18_210827) do
     t.datetime "updated_at", null: false
     t.index ["happy_thing_id"], name: "index_comments_on_happy_thing_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "daily_happy_email_deliveries", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "recipient_id", null: false
+    t.datetime "delivered_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_daily_happy_email_deliveries_on_recipient_id"
+    t.index ["user_id"], name: "index_daily_happy_email_deliveries_on_user_id"
   end
 
   create_table "friendships", force: :cascade do |t|
@@ -117,8 +127,10 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_18_210827) do
     t.float "longitude"
     t.bigint "category_id"
     t.boolean "share_location"
+    t.string "visibility", default: "public"
     t.index ["category_id"], name: "index_happy_things_on_category_id"
     t.index ["user_id"], name: "index_happy_things_on_user_id"
+    t.index ["visibility"], name: "index_happy_things_on_visibility"
   end
 
   create_table "users", force: :cascade do |t|
@@ -142,6 +154,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_18_210827) do
     t.string "unconfirmed_email"
     t.string "provider"
     t.string "uid"
+    t.string "timezone", default: "UTC"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -151,6 +164,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_18_210827) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "happy_things"
   add_foreign_key "comments", "users"
+  add_foreign_key "daily_happy_email_deliveries", "users"
+  add_foreign_key "daily_happy_email_deliveries", "users", column: "recipient_id"
   add_foreign_key "friendships", "users"
   add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "group_memberships", "groups"
