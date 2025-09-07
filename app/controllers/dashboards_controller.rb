aebc0e47d1795d1877c4be8241feaaddf3aa1b77
brunotho <brunothormaehlen@yahoo.com>
2025-09-07 11:41:17 +0200

    Fix @user_ids instance variable in dashboards controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -44,7 +44,7 @@ class DashboardsController < ApplicationController
     friend_ids = current_user.friends_and_friends_who_added_me_ids
     @user_ids = [current_user.id] + friend_ids
 
-    @happy_things_of_you_and_friends = HappyThing.where(user_id: user_ids)
+    @happy_things_of_you_and_friends = HappyThing.where(user_id: @user_ids)
                                                  .visible_to_user(current_user)
                                                  .order(created_at: :desc)
     fetch_happy_things_by_time(friend_ids)
