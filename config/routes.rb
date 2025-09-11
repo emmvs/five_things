2c2ad9e9f1e4a7d9b6aa490950d74091a251ffbb
brunotho <brunothormaehlen@yahoo.com>
2025-09-11 09:55:29 +0200

    Update /friends to go via users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show]
 
   get 'calendar', to: 'happy_things#calendar'
-  get 'friends', to: 'friends#index'
+  get 'friends', to: 'users#friends'
 
   # Happy Things
   get 'happy_things/:date', to: 'happy_things#show_by_date', as: :happy_things_by_date,
