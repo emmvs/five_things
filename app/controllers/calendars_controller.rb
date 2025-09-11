2740456fe2a31a9d7d5afff3e92e64bb1c048172
brunotho <brunothormaehlen@yahoo.com>
2025-09-11 08:47:04 +0200

    Add calendar#show; fix type in /calendar route; move calendar related helpers from dashboard_helper to calendar_helper

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class CalendarsController < ApplicationController
-  def show; end
+  def show
+    friend_ids = current_user.friends_and_friends_who_added_me_ids
+    @user_ids = [current_user.id] + friend_ids
+
+    @happy_things_of_you_and_friends = HappyThing.where(user_id: @user_ids).order(created_at: :desc)
+  end
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -67,35 +67,4 @@ module DashboardHelper
       full_moon: '🌕'
     }
   end
-
-  # Calender
-  def calculate_grid_size(number_of_emojis)
-    return 2 if number_of_emojis == 1
-
-    Math.sqrt(number_of_emojis).ceil
-  end
-
-  def calculate_emoji_size(number_of_emojis)
-    grid_size = calculate_grid_size(number_of_emojis)
-    case grid_size
-    when 2 then 1.1
-    when 3 then 0.8
-    when 4 then 0.5
-    else
-      raise "Failure in calculate_emoji_size: number_of_emojis = #{number_of_emojis}, grid_size = #{grid_size}"
-    end
-  end
-
-  def calculate_emoji_position(tile_number, number_of_emojis)
-    return '0px, 0px' if number_of_emojis > 4
-
-    case tile_number
-    when 1 then '-4px, -5px'
-    when 2 then '4px, -5px'
-    when 3 then '-4px, 5px'
-    when 4 then '4px, 5px'
-    else
-      raise "Failure in calculate_emoji_position: number_of_emojis = #{number_of_emojis}, tile_number = #{tile_number}"
-    end
-  end
 end
