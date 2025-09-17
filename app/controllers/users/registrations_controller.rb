fbc564fc0e542decd8afa00ac17d256aa263f6d4
brunotho <brunothormaehlen@yahoo.com>
2025-09-17 14:30:28 +0200

    Add users/registrations_controller for custom user update logic

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  def update_resource(resource, _params)
+    if updating_sensitive_info?
+      resource.update_with_password(user_params)
+    else
+      resource.update_without_password(user_params.except(:current_password))
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :avatar, :emoji, :email, :password, :password_confirmation,
+                                 :current_password, :email_opt_in, :location_opt_in)
+  end
+
+  def updating_sensitive_info?
+    user_params[:password].present?
+  end
+end
