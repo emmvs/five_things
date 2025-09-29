eaa5b04bf3d6c5c007091e4d6e931ee5e9c0fadf
emmvs <emma.ruenzel@1komma5grad.com>
2025-09-29 23:01:24 +0200

    Update rspec support f/ CI to stop failing

--- spec/support/geocoder.rb
+++ spec/support/geocoder.rb
@@ -4,6 +4,13 @@ require 'geocoder'
 
 Geocoder.configure(lookup: :test)
 
+# Default for any place string
+Geocoder::Lookup::Test.set_default_stub(
+  [{ 'coordinates' => [52.5173885, 13.3951309], 'address' => 'Berlin' }]
+)
+
+# Explicit Berlin stub for clarity
 Geocoder::Lookup::Test.add_stub(
-  'Berlin', [{ 'coordinates' => [52.510885, 13.3989367] }]
+  'Berlin',
+  [{ 'coordinates' => [52.5173885, 13.3951309], 'address' => 'Berlin' }]
 )
