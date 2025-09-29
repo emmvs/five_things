fbd2456339d34f04b39653e8d2fdfa69dcbec1f0
emmvs <emma.ruenzel@1komma5grad.com>
2025-09-29 18:21:48 +0200

    Add default & last line

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 OpenAI.configure do |config|
-  config.access_token = ENV.fetch('OPENAI_API_KEY')
+  config.access_token = ENV.fetch('OPENAI_API_KEY', 'FAKE_OPENAI_API_KEY')
 end
