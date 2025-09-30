b12e1cca33fbc6773099c4a41205ff36ba5519fc
brunotho <brunothormaehlen@yahoo.com>
2025-09-30 12:35:38 +0200

    Move title of happy thing form into the partial

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_08_20_184036) do
+ActiveRecord::Schema[7.0].define(version: 2025_09_01_152908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -157,6 +157,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_08_20_184036) do
     t.string "unconfirmed_email"
     t.string "provider"
     t.string "uid"
+    t.string "timezone", default: "UTC"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
