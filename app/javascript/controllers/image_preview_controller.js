764ab79dddb1f35599b01640428c0df7fbaa3b13
brunotho <brunothormaehlen@yahoo.com>
2025-10-01 13:28:20 +0200

    Improve profile edit page - Match happy thing form styling - Add translation calls - Add image_preview_controller for avatar changes - Improve styling for error messages - Improve styling for buttons

--- app/javascript/controllers/image_preview_controller.js
+++ app/javascript/controllers/image_preview_controller.js
@@ -1,12 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["input", "preview"]
+  static targets = ["input", "preview", "old_avatar"]
 
   preview() {
     const file = this.inputTarget.files[0]
     if (file) {
       this.previewTarget.src = URL.createObjectURL(file)
+      this.previewTarget.style.display = 'block'
+    }
+  }
+
+  edit() {
+    const file = this.inputTarget.files[0]
+    if (file) {
+      this.previewTarget.src = URL.createObjectURL(file)
+      this.old_avatarTarget.style.display = 'none'
       this.previewTarget.style.display = 'block'
     }
   }
