6bcd897736586fc79e3292ad698c429be1490e93
brunotho <brunothormaehlen@yahoo.com>
2025-10-02 15:27:43 +0200

    Fix style for happy_thing partial

--- app/helpers/happy_things_helper.rb
+++ app/helpers/happy_things_helper.rb
@@ -14,6 +14,15 @@ module HappyThingsHelper
     end
   end
 
+  def use_long_title_layout?(happy_thing)
+    base_threshold = 88
+    date_string_subtraction = friendly_date(happy_thing.start_time).length - 5
+    photo_emoji_subtraction = happy_thing.photo.attached? ? 6 : 0
+
+    threshold = base_threshold - photo_emoji_subtraction - date_string_subtraction
+    happy_thing.title.length >= threshold
+  end
+
   def grouped_visibility_options(user) # rubocop:disable Metrics/AbcSize
     group_options = user.groups.map { |g| ["🌟 #{g.name}", "group_#{g.id}"] }
     group_member_options = user.groups.flat_map do |group|
