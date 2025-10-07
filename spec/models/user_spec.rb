94108466930d24f9fdde0dcbe6d5e11c84453af9
emmvs <emma.ruenzel@1komma5grad.com>
2025-10-07 22:42:45 +0200

    rubocop happy

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User, type: :model do # rubocop:disable Metrics/BlockLength
   describe 'Validations' do
     it 'is valid with valid attributes' do
       user = build(:user)
