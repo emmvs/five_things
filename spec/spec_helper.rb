73452dfcb9469bcd5026ebae06b58c2f529aa799
brunotho <brunothormaehlen@yahoo.com>
2025-10-13 11:52:04 +0200

    test CI ENV

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -54,6 +54,9 @@ RSpec.configure do |config|
   #   # aliases for `it`, `describe`, and `context` that include `:focus`
   #   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
   config.filter_run_when_matching :focus
+  puts "🤡🤡🤡🤡🤡🤡🤡🤡🤡🤡🤡🤡"
+  puts "ENV['CI'] = #{ENV['CI'].inspect}"
+
   #
   #   # Allows RSpec to persist some state between runs in order to support
   #   # the `--only-failures` and `--next-failure` CLI options. We recommend
