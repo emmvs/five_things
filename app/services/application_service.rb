ee1ecaab78da4123e04b1df9f0d75bb1421907ee
emmvs <emma.ruenzel@1komma5grad.com>
2025-10-21 13:43:29 +0200

    Not sure what we did

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -3,9 +3,11 @@
 require 'httparty'
 require 'uri'
 
+# rubocop:disable Style/ArgumentsForwarding
 class ApplicationService
   # Entry point for service objects
   def self.call(*args, &block)
     new(*args, &block).call
   end
 end
+# rubocop:enable Style/ArgumentsForwarding
