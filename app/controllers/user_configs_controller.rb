156cbe3a9cabed636c302b3414809e5c2e6328dc
brunotho <brunothormaehlen@yahoo.com>
2025-10-29 22:09:34 +0100

    Add UserConfigs#update

--- app/controllers/user_configs_controller.rb
+++ app/controllers/user_configs_controller.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
 class UserConfigsController < ApplicationController
+  def update
+    @user_config = current_user.user_config
+
+    if @user_config.update(user_config_params)
+      head :ok
+    else
+      head :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_config_params
+    params.require(:user_config).permit(:install_prompt_shown)
+  end
 end
