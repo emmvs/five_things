c3efa444fcbc4687b5434e636394720858574304
brunotho <brunothormaehlen@yahoo.com>
2025-11-19 14:03:14 +0100

    Make rubocop happy

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -10,7 +10,7 @@ class DashboardsController < ApplicationController
     @random_poem = fetch_random_poem
     @random_quote = fetch_random_quote
     @happy_thing = HappyThing.new
-    user_config = current_user.user_config || current_user.create_user_config
+    current_user.user_config || current_user.create_user_config
     # @render_install_prompt = !user_config.install_prompt_shown
     @render_install_prompt = true
   end
