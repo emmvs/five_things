340918644518ddd626da93049f15e90f3e3fc11c
brunotho <brunothormaehlen@yahoo.com>
2025-11-23 18:38:06 +0100

    Fix user_configs_spec by reloading user

--- spec/requests/user_configs_spec.rb
+++ spec/requests/user_configs_spec.rb
@@ -13,11 +13,12 @@ RSpec.describe 'UserConfigs', type: :request do
       expect(@user.user_config.install_prompt_shown).to be(false)
       patch user_config_path, params: { user_config: { install_prompt_shown: true } }
       expect(response).to have_http_status(:ok)
-      expect(@user.user_config.install_prompt_shown).to be(true)
+      expect(@user.reload.user_config.install_prompt_shown).to be(true)
     end
 
     it "creates user_config for users that don't have one" do
       @user.user_config.destroy
+      @user.reload
       expect(@user.user_config).to be_nil
 
       patch user_config_path, params: { user_config: { install_prompt_shown: true } }
