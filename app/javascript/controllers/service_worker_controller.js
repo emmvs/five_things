e46907065a17449ea3428379ef7f8bdeb4464838
brunotho <brunothormaehlen@yahoo.com>
2025-11-23 14:51:43 +0100

    Remove service worker console logs

--- app/javascript/controllers/service_worker_controller.js
+++ app/javascript/controllers/service_worker_controller.js
@@ -2,14 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log('Service worker controller connected')
+    // console.log('Service worker controller connected')
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/service-worker.js')
         .then(registration => {
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          // console.log('ServiceWorker registration successful with scope: ', registration.scope);
         })
         .catch(err => {
-          console.log('ServiceWorker registration failed: ', err);
+          // console.log('ServiceWorker registration failed: ', err);
         });
     }
   }
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,14 @@
 self.addEventListener('install', event => {
-  console.log('Service worker installing...');
+  // console.log('Service worker installing...');
   // Put your assets caching logic here
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(clients.claim());
-  console.log('Service worker activated.');
+  // console.log('Service worker activated.');
 });
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch/ing:', event.request.url);
+  // console.log('Fetch/ing:', event.request.url);
   // Put your fetching and caching logic here
 });
